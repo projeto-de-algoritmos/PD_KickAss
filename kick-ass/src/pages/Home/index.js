aba7e699ba3480d02f57162aa1f310750ae0b978
saracampss <saracampxx@gmail.com>
2023-02-04 19:57:55 -0300

    Finalizando Home page

--- kick-ass/src/pages/Home/index.js
+++ kick-ass/src/pages/Home/index.js
@@ -1,5 +1,17 @@
+import './styles.css';
+import logo from '../../assets/kick-ass.png';
+import bigdaddy from '../../assets/big-daddy.webp';
+
 export default function Home() {
   return(
-    <h1>home</h1>
+    <div className='home' >
+      <img src={ logo } />
+      <p>Numa noite em Nova Iorque Big Daddy intercepta vários chamados da polícia 
+        e se prepara para sair em missão com Kick-Ass e Hit-Girl. 
+        <br/> Antes de tudo, Big Daddy utilizará um sistema para ajudá-lo a descobrir para onde ir primeiro
+        baseado no nível de urgência do chamado e distância de onde estão. </p>
+        <img className='big-daddy' src={ bigdaddy } />
+        <a href='/result'>Continuar</a>
+    </div>
   )
 }
